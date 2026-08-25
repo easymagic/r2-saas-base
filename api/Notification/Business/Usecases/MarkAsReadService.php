c99e40d9714d1b52572ce6e50648b707a023dcd2
alexander <alexander@watechnology.com>
2026-08-26 00:25:15 +0100

    Revert "main"
    This reverts commit d70a462c06ffbe928c34242873589361d9788852.

--- api/Notification/Business/Usecases/MarkAsReadService.php
+++ api/Notification/Business/Usecases/MarkAsReadService.php
@@ -4,7 +4,6 @@ namespace Notification\Business\Usecases;
 use Log\Business\Dtos\CreateLogDto;
 use Log\Business\LogServiceInterface;
 use Notification\Business\Dtos\MarkAsReadDto;
-use Notification\Data\NotificationEntity;
 use Notification\Data\NotificationRepositoryInterface;
 use Shared\Contracts;
 
@@ -27,24 +26,20 @@ class MarkAsReadService
         Contracts::requireEntityFound($notification, 'Notification');
 
         $authorized = (int) $notification->user_id === (int) $markAsReadDto->userId;
-        Contracts::requires($authorized, 'You are not authorized to mark this notification as read');
-
-        return $this->markAsRead($authorized, $notification, $markAsReadDto);
-    }
-
-    private function markAsRead(bool $authorized, NotificationEntity $notification, MarkAsReadDto $markAsReadDto){
-        if ($authorized){
+        if (!$authorized) {
             $this->logService->createLog(new CreateLogDto('notification_mark_as_read', json_encode([
                 'notification' => $notification,
                 'user_id' => $markAsReadDto->userId,
             ]), json_encode([
-                'notification' => $notification,
-                'user_id' => $markAsReadDto->userId,
-            ]), 'info'));    
-            $notification->read_at = date('Y-m-d H:i:s');
-            $notification->is_read = 1;
-            $notification->updated_at = date('Y-m-d H:i:s');
-            return $this->notificationRepository->save($notification);    
+                'status' => 'error',
+                'message' => 'You are not authorized to mark this notification as read',
+            ]), 'error'));
         }
+        Contracts::requires($authorized, 'You are not authorized to mark this notification as read');
+
+        $notification->read_at = date('Y-m-d H:i:s');
+        $notification->is_read = 1;
+        $notification->updated_at = date('Y-m-d H:i:s');
+        return $this->notificationRepository->save($notification);
     }
 }
